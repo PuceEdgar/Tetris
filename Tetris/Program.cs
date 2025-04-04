35912c5570363c81eaca10dafc14a51fb656dcc3
PuceEdgar <puce.edgar@gmail.com>
2025-04-04 14:29:31 +0900

    added line deletion

--- Tetris/Program.cs
+++ Tetris/Program.cs
@@ -9,13 +9,13 @@ namespace Tetris
 		static int xLine = 12;
 		static int yLine = 8;
 		static char pieceSymbol = 'X';
-
+		static List<int> fullLines = [];
 
 		static char[,] field = new char[xLine, yLine];
+
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, Tetris!");
-
+			Console.WriteLine("Hello, Tetris!");			
 
 			Console.CursorVisible = false;
 			var random = new Random();
@@ -28,21 +28,81 @@ namespace Tetris
 			{
 				if (isPieceLanded)
 				{
+					GetFullLines();
 					piece = new(1, random.Next(1, yLine-1));
 					isPieceLanded = false;
 				}
 
 				PopulateMatrix(ref piece);
-				PrintMatrix(field);
+				PrintMatrix();
+				Console.WriteLine();
+				
+
+				if (fullLines.Count > 0)
+				{
+					DeleteLines();					
+					fullLines = [];
+				}
+
+
 				Console.SetCursorPosition(0, 1);
 
 
-			}, null, 1000, 1000);
-
-
-
+			}, null, 1000, 100);
 
 			Console.ReadLine();
+		}
+
+		private static void DeleteLines()
+		{
+			foreach (var line in fullLines) 
+			{
+				for (int i = 1; i < yLine-1; i++)
+				{
+					field[line, i] = ' ';
+				}
+
+				MoveOtherPiecesDown(line-1);
+			}
+		}
+
+		private static void MoveOtherPiecesDown(int line)
+		{
+			for (int x = line; x > 0; x--)
+			{
+				for (int y = 0; y < yLine; y++)
+				{
+					if (field[x, y] == pieceSymbol)
+					{
+						field[x+1, y] = pieceSymbol;
+						field[x, y] = ' ';
+					}
+				}
+			}
+		}
+
+		private static void GetFullLines()
+		{
+			for (int i = 1; i < xLine-1; i++)
+			{
+				bool[] result = new bool[yLine-2]; 
+				for (int y = 1; y < yLine-1; y++)
+				{
+					if (field[i, y] == pieceSymbol)
+					{
+						result[y-1] = true;
+					}
+					else
+					{
+						break;
+					}
+				}
+
+				if (result.All(r => r))
+				{
+					fullLines.Add(i);
+				}
+			}
 		}
 
 		private static void CreateMatrixBase()
@@ -71,21 +131,16 @@ namespace Tetris
 			char nextXValue = field[++piece.Xposition, piece.Yposition];
 
 			isPieceLanded = nextXValue == '*' || nextXValue == pieceSymbol;
-
-			//else
-			//{
-			//	field[piece.Xposition, piece.Yposition] = piece.Symbol;
-			//}
 		}
 
-		private static void PrintMatrix(char[,] field)
+		private static void PrintMatrix()
 		{
 			// Print the populated matrix
 			for (int i = 0; i < field.GetLength(0); i++)
 			{
 				for (int j = 0; j < field.GetLength(1); j++)
 				{
-					Console.Write(field[i, j]); // Print each element with tab separation
+					Console.Write(field[i, j]); // Print each element
 				}
 				Console.WriteLine(); // New line after each row
 			}
